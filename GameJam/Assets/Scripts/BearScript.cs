835a1f4cb1ddea6ce79a048a9db296cba3b099c5
Kamil Kubiak <kamiljakubkubiak@gmail.com>
2017-02-18 00:09:17 +0100

    Adding events for bear

--- GameJam/Assets/Scripts/BearScript.cs
+++ GameJam/Assets/Scripts/BearScript.cs
@@ -3,16 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BearScript : MonoBehaviour {
+	public delegate void Gameplay();
+	public static event Gameplay PlayerFallDown;
+	public static event Gameplay PlayerRevived;
+	public float deathHeight = -5;
+	bool playerDead;
 	public float topSpeed=3f;
+	public Vector3 startPosition = Vector3.zero;
 	public Rigidbody rb;
+	void OnPlayerFallDown()
+	{
+		if (PlayerFallDown != null) {
+			PlayerFallDown ();
+		}
+	}
+
+	void Start()
+	{
+		PlayerFallDown += BearScript_PlayerFallDown;
+		PlayerRevived += BearScript_PlayerRevived;
+	}
+
+	void BearScript_PlayerRevived ()
+	{
+		transform.position = startPosition;
+		rb.velocity = Vector3.zero;
+		rb.useGravity = true;
+		playerDead = false;
+	}
+
+	void BearScript_PlayerFallDown ()
+	{
+		playerDead = true;
+		rb.velocity = Vector3.zero;
+		rb.useGravity = false;
+	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
+		if (playerDead) {
+			return;
+		}
 		rb.velocity = new Vector3(Mathf.Lerp(rb.velocity.x,(Input.acceleration.x * topSpeed),Mathf.Pow(0.5f,Time.deltaTime)),rb.velocity.y,rb.velocity.z);
 	}
 
-	void OnGUI()
+	void Update()
 	{
-		GUI.Label(new Rect(0,0,1000,80),"Gyroscope vector : " + Input.acceleration);    
+		if (transform.position.y <=deathHeight) {
+			OnPlayerFallDown ();
+		}
 	}
+
+
 }
