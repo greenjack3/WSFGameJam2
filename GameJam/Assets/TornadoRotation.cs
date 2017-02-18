9bf238ee833d0c8b764710fab320d6f2cb14d598
greenjack3 <jacyna20@gmail.com>
2017-02-18 19:56:14 +0100

    TrashSpawnTweak3

---
+++ GameJam/Assets/TornadoRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoRotation : MonoBehaviour {
+
+    public float yrot;
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, yrot, 0) * Time.deltaTime);
+    }
+}
