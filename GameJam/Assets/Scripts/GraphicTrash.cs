cf88b908671e0a064129f207570cf524dd197a45
Kamil Kubiak <kamiljakubkubiak@gmail.com>
2017-02-18 13:53:13 +0100

    new assets batch

--- GameJam/Assets/Scripts/GraphicTrash.cs
+++ GameJam/Assets/Scripts/GraphicTrash.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GraphicTrash : MonoBehaviour {
+public class GraphicTrash : MonoBehaviour
+{
 
     public float maxYdiff;
+    float maxY;
     public float minYdiff;
+    float minY;
     public float maxWobbleSpeed;
     public float minWobbleSpeed;
 
@@ -15,14 +18,17 @@ public class GraphicTrash : MonoBehaviour {
     float wobbleSpeed;
     float forwardSpeed;
 
-    void Start ()
+    void OnEnable()
     {
+        maxY = transform.position.y + maxYdiff;
+        minY = transform.position.y - minYdiff;
         wobbleSpeed = Random.Range(minWobbleSpeed, maxWobbleSpeed);
         forwardSpeed = Random.Range(minForwardSpeed, maxForwardSpeed);
-	}
-	
-	void Update ()
+    }
+
+    void Update()
     {
         transform.Translate(transform.forward * Time.deltaTime * forwardSpeed, Space.World);
+        transform.Translate(transform.up * Time.deltaTime * wobbleSpeed * Mathf.Sin(Time.timeSinceLevelLoad));
     }
 }
