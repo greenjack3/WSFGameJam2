9f72d12acae97373e1df793c1e3b7cb93f37844f
Kamil Kubiak <kamiljakubkubiak@gmail.com>
2017-02-18 02:51:24 +0100

    Functional Spawn Manager

--- GameJam/Assets/Scripts/SpawnManager.cs
+++ GameJam/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class SpawnManager : Singleton<SpawnManager> {
 
-	public Transform[] spawners;
+	public List<Transform> spawners;
 	public Obstacle[] obstaclePrefabs;
-	void Awake()
+	public float timeBetweenSpawns = 5f;
+
+	protected override void Awake ()
 	{
-		spawners = GetComponentsInChildren<Transform> ();
+		base.Awake ();
+		foreach (var item in GetComponentsInChildren<Transform>()) {
+			if (transform != item) {
+				spawners.Add (item);
+			}
+		}
+		BearScript.PlayerRevived += BearScript_PlayerRevived;
+		StartCoroutine (ContinousSpawning ());
 	}
 
+
+
+	void BearScript_PlayerRevived ()
+	{
+		StopAllCoroutines ();
+		StartCoroutine (ContinousSpawning ());
+	}
+
+	IEnumerator ContinousSpawning ()
+	{
+		float timer = 0;
+		while (true) {
+			timer += Time.deltaTime;
+			if (timer >= timeBetweenSpawns) {
+				timer = 0;
+				Spawn ();
+			}
+			yield return null;
+		}
+	}
+		
 	void Spawn()
 	{
-		
+		if (obstaclePrefabs.Length == 0 || spawners.Count == 0) {
+			return;
+		}
+		Obstacle nextObstacle = obstaclePrefabs [Random.Range (0, obstaclePrefabs.Length)];
+		nextObstacle.Spawn (spawners[Random.Range(0,spawners.Count)].position,Quaternion.identity);
 	}
 }
