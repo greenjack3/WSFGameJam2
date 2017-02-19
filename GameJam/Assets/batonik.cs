12e5d6ac7530cbbd6db2247d9f760eb488a26826
greenjack3 <jacyna20@gmail.com>
2017-02-19 03:10:03 +0100

    late night fine tune part3 (added buttons to main menu, and kraken scene)

---
+++ GameJam/Assets/batonik.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class batonik : MonoBehaviour {
+
+	public void startcik()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void krakenik()
+    {
+        SceneManager.LoadScene("uwolnićkrakena");
+    }
+}
